d7307de2fce172ff92e7cf7e0c40abe20401ec78
ZecaMonky <zacamonky@gmail.com>
2025-06-11 03:25:38 +0500

    fix2.4

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 
 import Footer from './components/footer';
 import Header from './components/header';
@@ -7,14 +7,9 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const inter = Inter({
+  subsets: ["latin", "cyrillic"],
+  variable: "--font-inter",
 });
 
 export const metadata = {
@@ -25,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <body>
+      <body className={inter.variable}>
         <Header />
         <main>{children}</main>
         <Footer />
